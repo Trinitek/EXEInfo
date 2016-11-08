9a01cf4aeb22dab5d62698b9dd424e8a6cde3843
Blake Burgess <trinitek@live.com>
2016-11-07 22:24:53 -0500

    Add NE segment table struct

--- exeinfo.c
+++ exeinfo.c
@@ -42,6 +42,9 @@ typedef struct
     short segment;
 } mz_reloc;
 
+/*
+	This NE executable header is also known as a "segmented EXE header".
+*/
 typedef struct
 {
 	unsigned short signature;		// "NE"
@@ -50,7 +53,7 @@ typedef struct
 	unsigned short entryTableOfs;	// Offset to entry table
 	unsigned short entryTableSize;	// Number of bytes in the entry table
 	unsigned long int crc;			// CRC checksum
-	unsigned short flags;			// Program load flags
+	unsigned short flags;			// Program load flags (See NE_FLAG_* flags)
 	unsigned short autoSeg;			// Segment number of auto data segment
 	unsigned short initialHeap;		// Number of bytes of heap to allocate at startup
 	unsigned short initialStack;	// Number of bytes of stack space to allocate at startup
@@ -70,8 +73,8 @@ typedef struct
 	unsigned short movableEntries;	// Number of movable entries in the entry table
 	unsigned short sectorAlignment;	// Logical sector alignment shift count
 	unsigned short resourceEntries;	// Number of resource entries
-	unsigned char exeType;			// Executable type
-	unsigned char otherFlags;		// Other program property flags
+	unsigned char exeType;			// Executable type (See NE_EXE_* defines)
+	unsigned char otherFlags;		// Other program property flags (See NE_EXE_OTHER_* flags)
 	unsigned short fastOfs;			// Sector offset to the fast-load area (Windows only)
 	unsigned short fastSize;		// Number of sectors in the fast-load area (Windows only)
 	unsigned short minCodeSwapSize;	// Minimum size of the code swap area (Windows only)
@@ -85,16 +88,37 @@ typedef struct
 #define NE_FLAG_LINKERROR		0x2000	// Errors during linking; will not load
 #define NE_FLAG_LIBRARY			0x8000	// Library module; SS:SP invalid, CS:IP points to an init procedure
 
-#define NE_OTHER_WIN2			0x01	// Windows 2.x app that can run in 3.x protected mode
-#define NE_OTHER_PROPORTIONAL	0x02	// Windows 2.x app that supports proportional fonts
-#define NE_OTHER_FASTLOAD		0x03	// Contains a fast-load area
-
 #define NE_EXE_UNKNOWN			0x00	// Unknown
 #define NE_EXE_OS2				0x01	// OS/2
 #define NE_EXE_WIN				0x02	// Windows
 #define NE_EXE_DOS4				0x03	// European MS-DOS 4.x
 #define NE_EXE_WIN386			0x04	// Windows 386
 #define NE_EXE_BOSS				0x05	// Borland OS Services
+
+#define NE_OTHER_WIN2			0x01	// Windows 2.x app that can run in 3.x protected mode
+#define NE_OTHER_PROPORTIONAL	0x02	// Windows 2.x app that supports proportional fonts
+#define NE_OTHER_FASTLOAD		0x03	// Contains a fast-load area
+
+/*
+	The segment table contains an entry for each segment in the executable file.
+	The number of segment table entries are defined in the NE header. The first
+	entry in the segment table is segment number 1.
+*/
+typedef struct
+{
+	unsigned short dataOfs;			// Logical-sector offset to the segment data
+	unsigned short dataSize;		// Length of the segment data in bytes
+	unsigned short flags;			// Flags (See NE_SEGTABLE_* flags)
+	unsigned short minAllocSize;	// Minimum allocation size of the segment in bytes
+} ne_segment_table;
+
+#define NE_SEGTABLE_TYPE_MASK	0x0007	// Segment-type field
+#define NE_SEGTABLE_CODE		0x0000	// Code-segment type
+#define NE_SEGTABLE_DATA		0x0001	// Data-segment type
+#define NE_SEGTABLE_MOVEABLE	0x0010	// Segment is not fixed
+#define NE_SEGTABLE_PRELOAD		0x0040	// Segment will be preloaded; read-only if data segment
+#define NE_SEGTABLE_RELOCINFO	0x0100	// Contains relocation records
+#define NE_SEGTABLE_DISCARD		0xF000	// Discard priority
 
 void printHelp()
 {
